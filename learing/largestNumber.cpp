00123f45932da80abe5a936cd9211d74aa61a9f2
uttom-kumar <uttom00781@gmail.com>
2025-12-17 23:54:34 +0600

    Check Largest Number

--- learing/largestNumber.cpp
+++ learing/largestNumber.cpp
@@ -8,10 +8,10 @@ int main (){
     cout << "Enter three numbers: ";
     cin >> num1 >> num2 >> num3;
 
-    if(num1>num2 || num1 > num3){
+    if(num1>num2 && num1 > num3){
         cout << "Num1 is the Largest Number : " << num1;
     }
-    else if(num2>num1 || num2>num3){
+    else if(num2>num1 && num2>num3){
         cout << "Num2 is the Largest Number : " << num2;
     }
     else{
