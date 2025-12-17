bd09fcc91b1d496979532bfbed520f6a26e5dabc
uttom-kumar <uttom00781@gmail.com>
2025-12-18 00:05:16 +0600

    check leap years

--- learing/leapYear.cpp
+++ learing/leapYear.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <conio.h>
 
+using namespace std;
+
+int main() {
+    int years;
+
+    while(1){
+        cout << "Enter Years : ";
+        cin >> years;
+
+        if((years%4 == 0 && years%100 != 0)|| (years%400 == 0)){
+            cout <<years << " is a leap years."<<endl;
+        }
+        else{
+            cout << years << " is not a leap years."<<endl;
+        }
+    }
+    getch();
+}
